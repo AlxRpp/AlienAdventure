f8c7aaf996c0593f326d5e610daff09a7373105a
AlxRpp <alexanderr26@outlook.de>
2025-04-03 20:43:48 +0200

    improved the mobile VolumeButton

--- scripts/audio.js
+++ scripts/audio.js
@@ -2,9 +2,9 @@ let mainMusic = new Audio('./assets/audio/mainSound.mp3');
 let jump = new Audio('./assets/audio/jump.mp3');
 let bottleBreak = new Audio('./assets/audio/bottleBreaking.mp3');
 let bottleCollect = new Audio('./assets/audio/bottleCollect.mp3');
-let chicken = new Audio ('./assets/audio/chicken.mp3');
+let chicken = new Audio('./assets/audio/chicken.mp3');
 let coinCollect = new Audio('./assets/audio/coin.mp3');
-let gameOver = new Audio ('./assets/audio/gameOver.mp3');
+let gameOver = new Audio('./assets/audio/gameOver.mp3');
 let hurtCharacter = new Audio('./assets/audio/hurtCharacter.mp3');
 let hurtBoss = new Audio('./assets/audio/hurtEndboss.mp3');
 let jumpCollision = new Audio('./assets/audio/jumpCollision.mp3');
@@ -38,7 +38,7 @@ let allAudios = [
 mainMusic.volume = .05;
 runCharacter.volume = .25;
 slash.volume = .2;
-runBoss.volume =.25;
+runBoss.volume = .25;
 bottleCollect.volume = .2;
 bottleBreak.volume = .2;
 jumpCollision.volume = .2;
@@ -48,17 +48,28 @@ slide.volume = .5;
 chicken.volume = .5;
 
 
-function muteAllSounds(){
+function muteAllSounds() {
+    const topIcon = document.getElementById('volumeTop')
+    const bottemIcon = document.getElementById('volumeBottom')
     const icon = document.getElementById('volume');
+    const mobileIcon = document.getElementById('mobileIcon');
     const volumeOn = './assets/images/volume_up.png';
     const volumeOff = './assets/images/volume_off.png';
     if (icon.src.includes('volume_up.png')) {
         icon.src = volumeOff;
-    }else {
+        topIcon.classList.add('border');
+        bottemIcon.classList.add('border');
+        mobileIcon.classList.add('border');
+
+    } else {
         icon.src = volumeOn;
+        topIcon.classList.remove('border');
+        bottemIcon.classList.remove('border');
+        mobileIcon.classList.remove('border');
+
     }
     allAudios.forEach(audio => {
-        audio.muted = ! audio.muted
+        audio.muted = !audio.muted
     });
 }
 
